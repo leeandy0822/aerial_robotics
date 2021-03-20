dc548439e0207d505bf2eda1024a7a973fb0ed67
leeandy0822 <leeandy90833@gmail.com>
2021-03-20 23:46:33 +0800

    rename package

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,7 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ubuntu/catkin_ws/build/gtest/cmake_install.cmake")
-  include("/home/ubuntu/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
+  include("/home/ubuntu/catkin_ws/build/hw1/cmake_install.cmake")
 
 endif()
 
---
+++ build/hw1/CMakeFiles/hw1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1.dir/src/hw1.cpp.o"
+  "/home/ubuntu/catkin_ws/devel/lib/homework/hw1.pdb"
+  "/home/ubuntu/catkin_ws/devel/lib/homework/hw1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
