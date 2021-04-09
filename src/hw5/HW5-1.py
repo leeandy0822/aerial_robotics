d054f110aa67a9fafda7ca6d693df3b6079f4f46
leeandy0822 <leeandy90833@gmail.com>
2021-04-10 01:06:26 +0800

    add HW5-1

---
+++ src/hw5/HW5-1.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+df = pd.read_excel (r'HW5-1.xls')
+
+# Get the columns
+out = df.loc[:, ["Output"]]
+A = df.loc[:,["Input 1","Input 2"]]
+
+# turn out and A to numpy array
+out = out.to_numpy()
+A = A.to_numpy()
+
+# add the column for constant
+A = np.c_[A, np.ones(50)]  
+
+# Let the function to be : out = A * X
+
+X = np.array([0,0,0])
+
+# Because A is not a square matrix, so I use the pseudo inverse matrix way
+A_pseudo = np.linalg.inv(((A.T).dot(A))).dot(A.T)
+
+# caculate
+X = A_pseudo.dot(out)
+print("Let the function be  ax + by +c = output")
+print("Final result: a = {}, b = {}, c = {}".format(round(X[0,0],2),round(X[1,0],2),round(X[2,0])))
